6068a5e912bb9f386c1ea02aa7ffc2b4735de68b
Shengzhang Wen <coffeesherk@gmail.com>
2014-08-24 17:11:07 +0800

    删除无用代码

--- library/src/main/java/org/gemini/httpengine/library/GMHttpService.java
+++ library/src/main/java/org/gemini/httpengine/library/GMHttpService.java
@@ -1,23 +1,9 @@
 package org.gemini.httpengine.library;
 
-import android.content.Context;
-import android.net.http.HttpResponseCache;
-import android.os.Handler;
-import android.os.HandlerThread;
-import android.os.Looper;
-import android.os.Message;
-
-import java.io.File;
-import java.io.IOException;
-import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.Executor;
-import java.util.concurrent.Executors;
 import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.SynchronousQueue;
-import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicInteger;
 
 public class GMHttpService {
 
@@ -34,34 +20,10 @@ public class GMHttpService {
 
 	private final Executor mService;
 
-	private final Handler.Callback mResponseCallBack;
-
-	/**
-	 * default thread that handler run on
-	 */
-	private HandlerThread mHandlerThread;
-	private Handler mCallbackHandler;
-
 	private GMHttpService() {
 		mService = new ThreadPoolExecutor(0, MAX_THREAD_NUM,
                 60L, TimeUnit.SECONDS,
-                new SynchronousQueue<Runnable>());
-		mResponseCallBack = new ResponseDataCallback();
-
-		mHandlerThread = new HandlerThread(TAG + "-HandlerThread");
-		mHandlerThread.start();
-		mCallbackHandler = new Handler(mHandlerThread.getLooper(),
-				mResponseCallBack);
-	}
-
-	/***
-	 * Set the callback looper for response. Just set it to parse response on
-	 * the right thread
-	 * 
-	 * @param looper Android Looper
-	 */
-	public void setCallbackLooper(Looper looper) {
-		this.mCallbackHandler = new Handler(looper, mResponseCallBack);
+                new LinkedBlockingQueue<Runnable>());
 	}
 
 	/***
@@ -80,26 +42,8 @@ public class GMHttpService {
         sInstance = new GMHttpService();
 	}
 
-	/**
-	 * receive the data of the runnable and response to the listener
-	 * 
-	 * @author GeminiWen
-	 * 
-	 */
-	private static class ResponseDataCallback implements Handler.Callback {
-		@Override
-		public boolean handleMessage(Message msg) {
-			ResponseUpdater runnable = (ResponseUpdater) msg.obj;
-			runnable.updateResponse();
-			return true;
-		}
-	}
 
-	interface ResponseUpdater {
-		public void updateResponse();
-	}
-
-	private class HttpRunnable implements Runnable, ResponseUpdater {
+	private class HttpRunnable implements Runnable {
 
 		private GMHttpRequest mHttpRequest;
 		private GMHttpResponse mHttpResponse;
@@ -121,12 +65,10 @@ public class GMHttpService {
 
 			// execute request and get response
             mHttpResponse = httpEngine.openUrl(mHttpRequest);
-			Message msg = mCallbackHandler.obtainMessage(0, this);
-			msg.sendToTarget();
+			this.updateResponse();
 		}
 
-		@Override
-		public void updateResponse() {
+		private void updateResponse() {
 			if (mHttpRequest.isCancel()) {
 				return;
 			}
@@ -145,7 +87,6 @@ public class GMHttpService {
 	 */
 	public void executeHttpMethod(GMHttpRequest httpRequest) {
 		Runnable runnable = new HttpRunnable(httpRequest);
-
 		mService.execute(runnable);
 	}
 
